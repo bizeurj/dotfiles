b732f18598eb6d1decb25fe414e607f85fb14b4e
Jonathan Bizeur <>
2019-01-28 18:14:12 -0500

    Add my current configuration files
    - eslintrc : ES Lint configuration
    - gitignore : Git ignore configuration
    - prettierrc : Prettier configuration
    - vscode.js : Visual Studio Code settings

---
+++ vscode.js
@@ -0,0 +1,67 @@
+{
+  "window.zoomLevel": -1,
+  "editor.formatOnSave": true,
+  // Enable per-language
+  "[html]": {
+    "editor.formatOnSave": true
+  },
+  "editor.fontFamily": "Fira Code, Operator Mono, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 18,
+  "editor.lineHeight": 28,
+  "editor.letterSpacing": 0.5,
+  "editor.fontLigatures": true,
+  "workbench.editor.tabSizing": "shrink",
+  "html.format.enable": false,
+  "html.format.preserveNewLines": true,
+  "files.trimTrailingWhitespace": true,
+  "editor.tabSize": 2,
+  // this isn't really underline but we hack it to be a thicker cursor
+  "editor.cursorStyle": "line",
+  "editor.cursorWidth": 3,
+  "editor.wordWrap": "on",
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontWeight": "400",
+  "editor.cursorBlinking": "solid",
+  "editor.colorDecorators": false,
+  "editor.minimap.enabled": false,
+  "vscode_custom_css.imports": [
+    "/Volumes/Macintosh HD/Users/wesbos/.vscodestyles.css"
+  ],
+  "workbench.activityBar.visible": true,
+  "workbench.sideBar.location": "right",
+  "gitlens.blame.line.enabled": true,
+  "editor.renderWhitespace": "none",
+  "editor.rulers": [80, 100, 120],
+  "editor.minimap.showSlider": "always",
+  "workbench.startupEditor": "newUntitledFile",
+  "extensions.ignoreRecommendations": false,
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true,
+    "**/.next": true
+  },
+  "workbench.colorTheme": "Night Owl",
+  "workbench.editor.tabCloseButton": "off",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "gitlens.advanced.messages": {
+    "suppressShowKeyBindingsNotice": true
+  },
+  "gitlens.historyExplorer.enabled": true,
+  "sync.gist": "f4b9d2082acf71366dc91acb2d9f5a2f",
+  "sync.host": "",
+  "sync.pathPrefix": "",
+  "sync.quietSync": false,
+  "sync.askGistName": false,
+  "sync.removeExtensions": true,
+  "sync.syncExtensions": true,
+  "sync.autoDownload": false,
+  "sync.autoUpload": false,
+  "sync.lastUpload": "2018-06-11T18:53:15.864Z",
+  "sync.lastDownload": "",
+  "sync.forceDownload": false
+}
